66577aa40787e0a240b56b5c6aec3cc59ea54dcd
Jessekay <kayjes00@gmail.com>
2025-11-15 13:11:34 +0200

    used useReducer to manage all kind of states with state variables to make the actual rendering much more easier

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import './css/styles.css';
 
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = userReducer(reducer, {})
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
